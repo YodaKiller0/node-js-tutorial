8a18d0b7f62548a28dd581a1122a4b128b8c0193
YodaKiller0 <lithumbandara885@gmail.com>
2026-06-07 20:35:10 +0530

    json data

--- app.js
+++ app.js
@@ -10,13 +10,37 @@ const server = http.createServer((request, response) => {
 
     let path = request.url;
     if (path === '/' || path.toLocaleLowerCase() === '/home'){
+
+        response.writeHead(200, {
+            'Content-type' : 'text/html',
+            'my-header' : 'hello world'
+        });
         response.end(html.replace('{{%CONTENT%}}', 'you are in home page'));
+
     }else if(path.toLocaleLowerCase() === '/about'){
+
+        response.writeHead(200, {
+            'Content-type' : 'text/html',
+            'my-header' : 'hello world'
+        });
         response.end(html.replace('{{%CONTENT%}}', 'you are in about page'));
+
     }else if (path.toLocaleLowerCase() === '/contact'){
+
+        response.writeHead(200, {
+            'Content-type' : 'text/html',
+            'my-header' : 'hello world'
+        });
         response.end(html.replace('{{%CONTENT%}}', 'you are in contact page'));
+
     }else{
+
+        response.writeHead(404, {
+            'Content-type' : 'text/html',
+            'my-header' : 'hello world'
+        });
         response.end(html.replace('{{%CONTENT%}}', 'error 404'));
+        
     }
 });
 
